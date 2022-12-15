09bc25a02c9efcb8b40f89b03a52ff457fd6f85b
Theron <Theron@DESKTOP-RD7GHET>
2022-12-15 15:38:47 +0200

    Add project files.

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
